56d569de726073e70d83b4b533e53ef6d18a36fe
chanon.cho <chanon.tod@gmail.com>
2021-03-02 17:26:55 +0700

    implement PostGateWayTest

--- src/test/java/com/example/kbtg/client/post/PostGatewayTest.java
+++ src/test/java/com/example/kbtg/client/post/PostGatewayTest.java
@@ -2,8 +2,15 @@ package com.example.kbtg.client.post;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -12,13 +19,30 @@ public class PostGatewayTest {
     @Autowired
     private PostGateway postGateway;
 
+    @MockBean
+    private RestTemplate restTemplate;
+
+    @Value("${post.api.url}")
+    private String postApiUrl;
+
     @Test
     public void success_with_id_1() {
+        PostResponse expected = new PostResponse();
+        expected.setUserId(1);
+        expected.setId(1);
+        expected.setTitle("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
+        expected.setBody("quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto");
 
+        Optional<PostResponse> actual = postGateway.getPostById(1);
+        assertTrue(actual.isPresent());
+        assertEquals(expected, actual.get());
     }
 
     @Test
     public void should_return_empty_when_exception_is_occured() {
-        
+        String url = String.format("%s/posts/%d", postApiUrl, 1);
+        when(restTemplate.getForObject(url, PostResponse.class)).thenThrow(new RestClientException(""));
+        Optional<PostResponse> actual = postGateway.getPostById(1);
+        assertTrue(!actual.isPresent());
     }
 }
